ba69f9dcb7af020422fb7b9cb487aee4c28b0316
WTDula <wtdula@gmail.com>
2022-03-16 14:14:47 -0500

    added capitalize_first_letter and tested

--- main.py
+++ main.py
@@ -16,3 +16,23 @@ def string_reversed(the_string):
 # 1 test
 print(string_reversed("Lunch!"))
 
+# 2. Capitalize letter
+#   a. Write code that takes a string as input and capitalize the first letter of each word. 
+#       Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
+
+# code as I understand it:
+# write a function that takes in a string and capitalize first letter of each word
+# if char == " "
+# easy -> string.title()
+# store new string in result string, return result string
+
+# prove space is a char to check for in for loop
+# print(len("this"))
+# print(len("this ")) # space is counted as a char
+
+def capitalize_first_letter(the_string):
+    return the_string.title()
+
+# test
+print(capitalize_first_letter("I strangely am more alert when i code"))
+
